8d8368157ba677121a7b8d4a94b7df860ce50543
ss1ngh <singhshreyansh2354@gmail.com>
2026-03-27 00:08:24 +0530

    Restore landing page changes from stash

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import FeaturesSection from "@/components/FeaturesSection";
 import Footer from "@/components/Footer";
+import PricingSection from "@/components/PricingSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <main className="flex-1">
         <HeroSection />
         <FeaturesSection />
+        <PricingSection />
       </main>
       <Footer />
     </>
